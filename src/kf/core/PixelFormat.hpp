be96d79be5b4df9082d19d553d3b9eaaff290c44
JamahaW <153020210+JamahaW@users.noreply.github.com>
2026-01-24 01:02:12 +0300

    PixelFormat::RGB565 is Bit Endian (Actually)

--- src/kf/core/PixelFormat.hpp
+++ src/kf/core/PixelFormat.hpp
@@ -10,7 +10,7 @@ namespace kf {
 /// @brief Pixel format enumeration
 enum class PixelFormat : u8 {
     Monochrome,///< 1-bit monochrome format (1 bit per pixel)
-    RGB565,    ///< 16-bit RGB565 format (5-6-5 bits per channel)
+    RGB565,    ///< 16-bit BIG ENDIAN RGB565 format (5-6-5 bits per channel)
 };
 
 }// namespace kf
